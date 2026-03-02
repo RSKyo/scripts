1a73ff0a656298d6acbb7689661fe197528fb089
RSKyo <nimeimiao@gmail.com>
2026-03-03 03:58:51 +0800

    meta

--- lib/yt/video/meta.source.sh
+++ lib/yt/video/meta.source.sh
@@ -3,10 +3,11 @@
 # shellcheck disable=SC1091,SC2034,SC2154
 
 # Prevent multiple sourcing
-[[ -n "${__YT_VIDEO_META_SOURCED+x}" ]] && return 0
-__YT_VIDEO_META_SOURCED=1
+# [[ -n "${__YT_VIDEO_META_SOURCED+x}" ]] && return 0
+# __YT_VIDEO_META_SOURCED=1
 
 declare -Ar YT_VIDEO_META_FILTER_MAP=(
+  [id]='.id // empty'
   [title]='.title // empty'
   [duration]='.duration // 0'
   [description]='.description // empty'
@@ -16,10 +17,12 @@ declare -Ar YT_VIDEO_META_FILTER_MAP=(
 source "$LIB_DIR/file.source.sh"
 source "$LIB_DIR/yt/video/url.source.sh"
 
-yt_video_meta_download() {
-  local input="${1:?yt_video_meta_download: missing id or url}"
+yt_video_meta() {
+  local input="${1:?yt_video_meta: missing url}"
   shift
-  local meta_dir="${YT_CACHE_DIR}/meta"
+  local dir="${YT_CACHE_DIR}"
+  local sub_dir='meta'
+  local field
   local refresh=0
 
   while [[ $# -gt 0 ]]; do
@@ -27,8 +30,21 @@ yt_video_meta_download() {
       --dir) 
         shift
         [[ $# -ge 1 ]] || return 2
-        meta_dir="$1"
+        dir="$1"
         shift
+        ;;
+      --sub-dir) 
+        shift
+        [[ $# -ge 1 ]] || return 2
+        sub_dir="$1"
+        shift
+        ;;
+      --field) 
+        shift
+        [[ $# -ge 1 ]] || return 2
+        field="$1"
+        shift
+        [[ -n "${YT_VIDEO_META_FILTER_MAP[$field]}" ]] || return 2
         ;;
       --refresh) 
         shift
@@ -46,8 +62,12 @@ yt_video_meta_download() {
   }
   url="$(yt_video_url_canonical "$id")" || return 2
 
+  dir=${dir%/}
+  sub_dir=${sub_dir%/}
+  sub_dir=${sub_dir#/}
+
   local meta_name="${id}.meta.json"
-  local meta_path="$meta_dir/$meta_name"
+  local meta_path="${dir}/${sub_dir}/$meta_name"
 
   if (( refresh )) || [[ ! -s "$meta_path" ]]; then
     "$yt_dlp" \
@@ -55,7 +75,7 @@ yt_video_meta_download() {
       --skip-download \
       --dump-json \
       "$url" 2>/dev/null |
-    file_write "$meta_name" --dir "$meta_dir"
+    file_write "$meta_name" --dir "${dir}/${sub_dir}"
 
     if [[ -s "$meta_path" ]]; then
       logi "Meta written: $meta_path"
@@ -66,58 +86,12 @@ yt_video_meta_download() {
   else
     logi "Meta exist: $meta_path"
   fi
-}
 
-yt_video_meta() {
-  local input="${1:?yt_video_meta: missing url}"
-  local field="${2:?yt_video_meta: missing field}"
-  shift 2
-  local meta_dir="${YT_CACHE_DIR}/meta"
-  local refresh=0
+  if [[ -n "$field" ]]; then
+    local filter field_value
+    filter="${YT_VIDEO_META_FILTER_MAP[$field]}"
+    field_value="$("$jq_bin" -r "$filter" "$meta_path")" || return 1
 
-  while [[ $# -gt 0 ]]; do
-    case "$1" in
-      --dir) 
-        shift
-        [[ $# -ge 1 ]] || return 2
-        meta_dir="$1"
-        shift
-        ;;
-      --refresh) 
-        shift
-        refresh=1
-        ;;
-      --) shift; break ;;
-      *) return 2 ;;
-    esac
-  done
-
-  local id url
-  id="$(yt_video_url_id "$input")" || {
-    loge "Invalid input: $input"
-    return 2
-  }
-  url="$(yt_video_url_canonical "$id")" || return 2
-
-  local filter
-  filter="${YT_VIDEO_META_FILTER_MAP[$field]}"
-  [[ -n "$filter" ]] || {
-    loge "Unknown meta field: $field"
-    return 2
-  }
-
-  local meta_name="${id}.meta.json"
-  local meta_path="$meta_dir/$meta_name"
-
-  if (( refresh )) || [[ ! -s "$meta_path" ]]; then
-    local args=(--dir "$meta_dir")
-    (( refresh )) && args+=(--refresh)
-
-    yt_video_meta_download "$url" "${args[@]}"
+    printf '%s\n' "$field_value"
   fi
-
-  local field_value
-  field_value="$("$jq_bin" -r "$filter" "$meta_path")" || return 1
-
-  printf '%s\n' "$field_value"
 }
