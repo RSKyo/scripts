7e300d7e1dd53879e627e19d73570d612100ddf4
RSKyo <nimeimiao@gmail.com>
2026-03-03 17:44:38 +0800

    meta temp

--- lib/yt/video/meta.source.sh
+++ lib/yt/video/meta.source.sh
@@ -17,57 +17,70 @@ declare -Ar YT_VIDEO_META_FILTER_MAP=(
 source "$LIB_DIR/file.source.sh"
 source "$LIB_DIR/yt/video/url.source.sh"
 
-yt_video_meta() {
-  local input="${1:?yt_video_meta: missing url}"
+__yt_video_meta_derive() {
+  local input="${1:?yt_video_meta_path: missing url}"
   shift
   local dir="${YT_CACHE_DIR}"
   local sub_dir='meta'
-  local field
-  local refresh=0
 
   while [[ $# -gt 0 ]]; do
     case "$1" in
-      --dir) 
-        shift
-        [[ $# -ge 1 ]] || return 2
-        dir="$1"
-        shift
-        ;;
-      --sub-dir) 
-        shift
-        [[ $# -ge 1 ]] || return 2
-        sub_dir="$1"
-        shift
-        ;;
-      --field) 
-        shift
-        [[ $# -ge 1 ]] || return 2
-        field="$1"
-        shift
-        [[ -n "${YT_VIDEO_META_FILTER_MAP[$field]}" ]] || return 2
-        ;;
-      --refresh) 
-        shift
-        refresh=1
-        ;;
+      --dir) shift; [[ $# -ge 1 ]] || return 2; dir="$1"; shift ;;
+      --sub-dir) shift; [[ $# -ge 1 ]] || return 2; sub_dir="$1"; shift ;;
       --) shift; break ;;
       *) return 2 ;;
     esac
   done
 
   local id url
-  id="$(yt_video_url_id "$input")" || {
-    loge "Invalid input: $input"
-    return 2
-  }
+  id="$(yt_video_url_id "$input")" || { loge "Invalid input: $input"; return 2; }
   url="$(yt_video_url_canonical "$id")" || return 2
 
   dir=${dir%/}
   sub_dir=${sub_dir%/}
   sub_dir=${sub_dir#/}
 
-  local meta_name="${id}.meta.json"
-  local meta_path="${dir}/${sub_dir}/$meta_name"
+  local name path
+  name="${id}.meta.json"
+  path="${dir}/${sub_dir}/${name}"
+
+  local sep=$'\x1f'
+  printf '%s%s%s%s%s%s%s\n' "$id" "$STRING_SEP" "$url" "$STRING_SEP" "$name" "$STRING_SEP" "$path" 
+}
+
+yt_video_meta_download() {
+  local input="${1:?yt_video_meta_download: missing url}"
+  shift
+  local dir="${YT_CACHE_DIR}"
+  local sub_dir='meta'
+  local refresh=0
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --dir) shift; [[ $# -ge 1 ]] || return 2; dir="$1"; shift ;;
+      --sub-dir) shift; [[ $# -ge 1 ]] || return 2; sub_dir="$1"; shift ;;
+      --refresh) shift; refresh=1 ;;
+      --) shift; break ;;
+      *) return 2 ;;
+    esac
+  done
+
+
+  local id url name path
+  IFS=$'\x1f' read -r id url name path < <(\
+    __yt_video_meta_derive "$input" \
+      --dir "$dir" --sub-dir "$sub_dir" \
+  ) || return 2
+  
+  meta_path="$(__yt_video_meta_derive "$id" --dir "$dir" --sub-dir "$sub_dir")" || return $?
+
+
+  local id url meta_path meta_name
+
+  id="$(yt_video_url_id "$input")" || { loge "Invalid input: $input"; return 2; }
+  url="$(yt_video_url_canonical "$id")" || return 2
+  
+  meta_name="${meta_path##*/}"
 
   if (( refresh )) || [[ ! -s "$meta_path" ]]; then
     "$yt_dlp" \
@@ -86,12 +99,36 @@ yt_video_meta() {
   else
     logi "Meta exist: $meta_path"
   fi
+}
 
-  if [[ -n "$field" ]]; then
-    local filter field_value
-    filter="${YT_VIDEO_META_FILTER_MAP[$field]}"
-    field_value="$("$jq_bin" -r "$filter" "$meta_path")" || return 1
+yt_video_meta() {
+  local input="${1:?yt_video_meta: missing url}"
+  local field="${2:?yt_video_meta: missing meta field}"
+  shift 2
+  local dir="${YT_CACHE_DIR}"
+  local sub_dir='meta'
+  local refresh=0
 
-    printf '%s\n' "$field_value"
-  fi
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --dir) shift; [[ $# -ge 1 ]] || return 2; dir="$1"; shift ;;
+      --sub-dir) shift; [[ $# -ge 1 ]] || return 2; sub_dir="$1"; shift ;;
+      --refresh) shift; refresh=1 ;;
+      --) shift; break ;;
+      *) return 2 ;;
+    esac
+  done
+
+  [[ -n "${YT_VIDEO_META_FILTER_MAP[$field]}" ]] || return 2
+
+  yt_video_meta_download "$input" \
+    --dir "$dir" \
+    --sub-dir "$sub_dir" \
+    --refresh "$refresh" || return $?
+
+  local meta_path filter
+  meta_path="$(yt_video_meta_path "$input" --dir "$dir" --sub-dir "$sub_dir")" || return $?
+  filter="${YT_VIDEO_META_FILTER_MAP[$field]}"
+
+  "$jq_bin" -r "$filter" "$meta_path" || return 1
 }
